f46d70d1ee33cf6fd4384c82f1769979c0eb7f7a
feriafrianto <feriafrianto7@gmail.com>
2021-01-07 16:24:54 +0700

    Percobaan 8 Done

--- client/main.js
+++ client/main.js
@@ -22,5 +22,12 @@ Template.images.helpers({images:Images.find()});
 Template.images.events({
 	'click .js-image':function(event){
 		$(event.target).css("width","200px");
+	},
+	'click .js-del-image':function(event){
+		var image_id = this._id;
+		console.log(image_id);
+		$("#"+image_id).hide('slow',function(){
+			Images.remove({"_id":image_id});
+		})
 	}
 })
